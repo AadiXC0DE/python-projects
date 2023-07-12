9be352c157fbea3e27254ede2d94a357bce8eb89
AadiXC0DE <aadityaz2077@gmail.com>
2023-07-12 11:52:00 +0530

    temperature converter

--- temperature.py
+++ temperature.py
@@ -9,3 +9,27 @@ def celsius_to_kelvin(celsius):
 def fahrenheit_to_celsius(fahrenheit):
     celsius = (fahrenheit - 32) * 5/9
     return celsius
+
+def main():
+    print("Temperature Converter")
+    print("1. Celsius to Fahrenheit")
+    print("2. Celsius to Kelvin")
+    print("3. Fahrenheit to Celsius")
+
+    choice = int(input("Enter your choice: "))
+    temperature = float(input("Enter the temperature: "))
+
+    if choice == 1:
+        converted_temperature = celsius_to_fahrenheit(temperature)
+        print(f"{temperature}°C is equal to {converted_temperature}°F")
+    elif choice == 2:
+        converted_temperature = celsius_to_kelvin(temperature)
+        print(f"{temperature}°C is equal to {converted_temperature}K")
+    elif choice == 3:
+        converted_temperature = fahrenheit_to_celsius(temperature)
+        print(f"{temperature}°F is equal to {converted_temperature}°C")
+    else:
+        print("Invalid choice.")
+
+if __name__ == "__main__":
+    main()
