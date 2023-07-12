fbb5005338a032f18d749ded37bf8f33eae7e124
AadiXC0DE <aadityaz2077@gmail.com>
2023-07-12 11:51:34 +0530

    defined functions temperature

---
+++ temperature.py
@@ -0,0 +1,11 @@
+def celsius_to_fahrenheit(celsius):
+    fahrenheit = (celsius * 9/5) + 32
+    return fahrenheit
+
+def celsius_to_kelvin(celsius):
+    kelvin = celsius + 273.15
+    return kelvin
+
+def fahrenheit_to_celsius(fahrenheit):
+    celsius = (fahrenheit - 32) * 5/9
+    return celsius
