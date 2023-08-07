fe0ce4e249c1192213d466b6f17e27d0feb2db94
AadiXC0DE <aadityaz2077@gmail.com>
2023-08-07 22:26:39 +0530

    findfibo

---
+++ findfibo.py
@@ -0,0 +1,50 @@
+def is_perfect_square(n):
+    sqrt_n = int(n ** 0.5)
+    return sqrt_n * sqrt_n == n
+
+def is_fibonacci_number(number):
+    if number < 0:
+        return False
+
+    return is_perfect_square(5 * number * number + 4) or is_perfect_square(5 * number * number - 4)
+
+def display_instructions():
+    print("Welcome to Find out, Fibonacci!")
+    print("This game checks whether a given number belongs to the Fibonacci sequence.")
+    print("You can also explore a portion of the Fibonacci sequence.")
+    print("Enter 'q' to quit the game.\n")
+
+def main():
+    display_instructions()
+
+    while True:
+        choice = input("Do you want to check a number (1) or explore a portion (2) of the Fibonacci sequence? ")
+
+        if choice == '1':
+            number = int(input("Enter a number to check if it belongs to the Fibonacci sequence: "))
+            if is_fibonacci_number(number):
+                print(f"{number} is a Fibonacci number.\n")
+            else:
+                print(f"{number} is not a Fibonacci number.\n")
+
+        elif choice == '2':
+            n_terms = int(input("Enter the number of terms you want to explore: "))
+            a, b = 0, 1
+            count = 0
+
+            print("Fibonacci sequence:")
+            while count < n_terms:
+                print(a, end=' ')
+                a, b = b, a + b
+                count += 1
+            print("\n")
+
+        elif choice.lower() == 'q':
+            print("Thank you for playing Find out, Fibonacci!")
+            break
+
+        else:
+            print("Invalid choice. Please enter '1', '2', or 'q'.\n")
+
+if __name__ == "__main__":
+    main()
