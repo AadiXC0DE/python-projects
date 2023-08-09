8cbd600c390442f15b45badbd4ab61ffd12eaf4c
AadiXC0DE <aadityaz2077@gmail.com>
2023-08-09 22:50:39 +0530

    added features

--- speechtotext.py
+++ speechtotext.py
@@ -1,29 +1,33 @@
 import speech_recognition as sr
-#pip install SpeechRecognition
-
+import time
 
 def speech_to_text():
-    
     r = sr.Recognizer()
 
-    
-    with sr.Microphone() as source:
-        print("Speak something...")
-        audio = r.listen(source)
+    while True:
+        with sr.Microphone() as source:
+            print("Speak something...")
+            audio = r.listen(source)
 
-    try:
+        try:
+            print("Recognizing...")
+            text = r.recognize_google(audio)
+            print("You said: " + text)
+
+            timestamp = time.strftime("%Y-%m-%d_%H-%M-%S")
+            filename = f"output_{timestamp}.txt"
+            
+            with open(filename, "w") as file:
+                file.write(text)
+            print(f"Text saved to '{filename}'.")
+
+        except sr.UnknownValueError:
+            print("Sorry, I couldn't understand your speech.")
+        except sr.RequestError as e:
+            print("Sorry, an error occurred. {0}".format(e))
         
-        text = r.recognize_google(audio)
-        print("You said: " + text)
-
-        # Write the recognized text to a file
-        with open("output.txt", "w") as file:
-            file.write(text)
-        print("Text saved to 'output.txt'.")
-
-    except sr.UnknownValueError:
-        print("Sorry, I couldn't understand your speech.")
-    except sr.RequestError as e:
-        print("Sorry, an error occurred. {0}".format(e))
+        choice = input("Do you want to continue recording? (y/n): ")
+        if choice.lower() != 'y':
+            break
 
 speech_to_text()
